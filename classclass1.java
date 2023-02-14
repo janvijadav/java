dfecbc54feb8af2a592954bc29f3b70db7861e08
janvijadav <110669936+janvijadav@users.noreply.github.com>
2023-02-14 18:00:50 +0530

    Add files via upload

---
+++ classclass1.java
@@ -0,0 +1,19 @@
+public class classclass1
+{
+    public static void main(String[] args) throws ClassNotFoundException
+    {
+        classclass1 = class.forname("java.lang.string");
+        classclass2 = int.class;
+        System.out.println("Class represented by class1: ");
+        System.out.println(class1.tostring());
+        System.out.println("Class represented by class2: ");
+        System.out.println(class2.tostring());
+        String S ="javapoint";
+        int i = 10;
+        Integer j = new Integer(i);
+        boolean b1 = class1.isIntance(s);
+        boolean b2 = class2.isIntance(i);
+        System.out.println("is p instance of string: "+b1);
+        System.out.println("is j instance of integar: "+b2);
+    }
+}
